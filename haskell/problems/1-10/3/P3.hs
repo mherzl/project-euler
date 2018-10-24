25e6b7ce8dc3acb02d072ce516514a18ce3ca5f0
matthew <matthew.herzl@gmail.com>
2018-10-24 23:26:05 +0000

    P3.hs done

---
+++ haskell/problems/1-10/3/P3.hs
@@ -0,0 +1,24 @@
+
+
+import Data.Maybe (fromMaybe)
+
+n :: Integer
+n = 600851475143
+
+main = print $ largestPrimeFactor n
+
+largestPrimeFactor :: Integer -> Integer
+largestPrimeFactor x
+  | x <= 1 = 1
+  | leastPrimeDivisor == x = x
+  | otherwise = largestPrimeFactor (div x leastPrimeDivisor)
+  where candidateDivisors = takeWhile (\d -> d*d <= x) (2:[1+2*d | d <- [1..]])
+        leastPrimeDivisor = fromMaybe x (first (\d -> mod x d == 0) candidateDivisors)
+
+
+first :: (a -> Bool) -> [a] -> Maybe a
+first _ [] = Nothing
+first p (x:xs)
+  | p x = Just x
+  | otherwise = first p xs
+
