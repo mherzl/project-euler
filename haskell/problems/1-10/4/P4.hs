03ad83d50f9eaabf458487a5638db6bf7eb2e83c
matthew <matthew.herzl@gmail.com>
2018-10-26 01:36:32 +0000

    P4.hs is working

--- haskell/problems/1-10/4/P4.hs
+++ haskell/problems/1-10/4/P4.hs
@@ -1,33 +1,38 @@
 
 
 
+import Data.Maybe (fromJust)
+import Data.List (find)
+import Control.Monad (ap)
 
-data DigitList = DigitList [Int]
 
-isPalindrome :: DigitList -> Bool
-isPalindrome (DigitList l) = l == reverse l
+threeDigitPairs :: [(Int, Int)]
+threeDigitPairs = takeWhile bothThreeDigit $ iterate increment (1,1)
+  where
+    bothThreeDigit :: (Int, Int) -> Bool
+    bothThreeDigit (x, y) = x<1000 && y<1000
+    increment :: (Int, Int) -> (Int, Int)
+    increment (x, y)
+      | x <= y = (x+1, 1)
+      | otherwise = (x, y+1)
 
-toDigitList :: Int -> DigitList
-toDigitList n
-  | n < 10 = DigitList [n]
-  | otherwise = (mod n 10) `cons` (toDigitList $ div n 10)
-  where cons :: Int -> DigitList -> DigitList
-        cons x (DigitList xs) = DigitList (x:xs)
+products :: [(Int, Int)] -> [Int]
+products = map $ uncurry (*)
 
-allUnorderedPairs :: [(Int, Int)]
-allUnorderedPairs = iterate advance (1,1)
-  where advance :: (Int, Int) -> (Int, Int)
-        advance (x, y)
-          | x>y = (x, y+1)
-          | otherwise = (x+1,1)
+palindromes :: [Int] -> [Int]
+palindromes = filter (isPalindrome . toDigitList)
+  where
+    isPalindrome :: [Int] -> Bool
+    isPalindrome = ap (==) reverse
+    toDigitList :: Int -> [Int]
+    toDigitList n
+      | n < 10 = [n]
+      | otherwise = mod n 10 : toDigitList (div n 10)
 
-allProductsOfThreeDigitNumbers :: [Int]
-allProductsOfThreeDigitNumbers = (\(x,y) -> x*y) <$> takeWhile (\(x,_) -> x<1000) allUnorderedPairs
+answer :: Int
+answer = maximum $ palindromes $ products $ threeDigitPairs
 
-palindromes :: [Int]
-palindromes = filter (isPalindrome . toDigitList) allProductsOfThreeDigitNumbers
-
-main = print $ maximum palindromes
+main = print $ answer
 
 
 
